bb15e6e32f9c1ed009d4980628bc0ad5d812f65e
Keallan Saunders <khazsaunders@hotmail.com>
2021-04-01 18:03:04 +0200

    First Commit

---
+++ src/main/java/za/ac/cput/imtired/Division.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package za.ac.cput.imtired;
+
+/**
+ *
+ * @author******* Keallan Saunders 219169357***********
+ */
+public class Division {
+  private int marble(int a, int b, int c){
+      a = 1000;
+      b = 500;
+      c = 500;
+      
+   return a / b * c;   
+      
+      
+  }
+}
